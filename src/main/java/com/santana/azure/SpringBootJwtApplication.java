b422bb964c68fe5b1e227bf9b5054b0e7a32b237
Eder Sant'ana <edersana@gmail.com>
2022-02-28 12:10:58 -0300

    Autenticação e autorização utilizando Azure

---
+++ src/main/java/com/santana/azure/SpringBootJwtApplication.java
@@ -0,0 +1,19 @@
+package com.santana.azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@SpringBootApplication
+public class SpringBootJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtApplication.class, args);
+	
+	}
+	
+
+}
